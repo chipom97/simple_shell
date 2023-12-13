d01d01c6f6c427279500f6406954e7fa7f2ab660
Chipo Mazhani <chipomazhani@gmail.com>
2023-12-13 13:20:02 -0800

    Adding shell files

---
+++ shell.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the simple shell
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Exit status
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+return (0);
+}
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+int main(int argc, char *argv[]);
+
+
+#endif /* SHELL_H */
