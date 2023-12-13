55f3cfb8e064f243206fd6158c3d6c113e29dd4d
Chipo Mazhani <chipomazhani@gmail.com>
2023-12-13 13:54:38 -0800

    Simple shell 0.1

--- shell.h
+++ shell.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /* Function prototypes */
 int main(int argc, char *argv[]);
