ab2b43816dd8bb6842c9c466a2bdd1a639da97e4
Chipo Mazhani <chipomazhani@gmail.com>
2024-03-06 23:45:20 -0800

    added header file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#endif /* _SHELL_H_ */
