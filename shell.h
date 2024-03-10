4623293a67a7a2717b7d24f1341bfcdc0996fe95
Chipo Mazhani <chipomazhani@gmail.com>
2024-03-09 23:22:01 -0800

    removed file

--- shell.h
+++ shell.h
@@ -14,7 +14,6 @@ void run_shell(void);
 void tokenize_input(char *line, char **args);
 void handle_builtin(char **args);
 void create_child_process(char **args);
-void initialize_shell(void);
 void handle_ls_command(char **args);
 void execute_command(char **args);
 
