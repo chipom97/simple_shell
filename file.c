c34bf38835b36d183ee877ec1877e25019e0f48d
Chipo Mazhani <chipomazhani@gmail.com>
2024-03-09 23:48:08 -0800

    check 1

---
+++ file.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+#define MAX_LINE_LENGTH 80
+#define MAX_ARGS 10
+
+/**
+* main - check the code
+*
+* Return: (0) Success.
+*/
+
+int main(void)
+{
+printf("Hi");
+return (0);
+}
